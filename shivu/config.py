256af3589afce5ce75d508575ce9c9d5f06248f1
ZealBots07 <zealanime@protonmail.com>
2024-08-10 18:50:37 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "6170772579"
+    sudo_users = "6170772579", "6765826972"
+    GROUP_ID = -1002133033983
+    TOKEN = "7298455205:AAFUCtuFf2lTOJAXYcgL0tiXHDVglCKUp3s"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "zealanime_fun"
+    UPDATE_CHAT = "zealanime_fun"
+    BOT_USERNAME = "Husbando_zealBot"
+    CHARA_CHANNEL_ID = "-1002133033983"
+    api_id = 28408158
+    api_hash = "ddda6703b42211276b28252aca309a2d"
 
     
 class Production(Config):
